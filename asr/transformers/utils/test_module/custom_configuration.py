806ab051484c1160dcdf6e6fb659f433dcbb689f
Vladimir Albrekht <sluminat2@gmail.com>
2025-12-10 21:37:15 +0500

    Deployed 285fd18 with MkDocs version: 1.6.1

--- asr/transformers/utils/test_module/custom_configuration.py
+++
@@ -1,9 +0,0 @@
-from transformers import PreTrainedConfig
-
-
-class CustomConfig(PreTrainedConfig):
-    model_type = "custom"
-
-    def __init__(self, attribute=1, **kwargs):
-        self.attribute = attribute
-        super().__init__(**kwargs)
